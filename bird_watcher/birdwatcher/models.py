c5836070f4d6058ffe026cb4db2b3ca803c73910
Stephane Latil <stephanelatil@gmail.com>
2024-03-10 18:27:00 +0100

    Fixes autofield

--- bird_watcher/birdwatcher/models.py
+++ bird_watcher/birdwatcher/models.py
@@ -2,15 +2,13 @@ from django.db import models
 from django.conf import settings
 
 class Tag(models.Model):
-    id = models.BigAutoField()
     name = models.CharField(max_length=32)
     #videos = related_name
     
 class Video(models.Model):
-    id = models.BigAutoField()
     video_file = models.FilePathField(path=settings.VIDEOS_DIRECTORY)
     thumbnail_file = models.ImageField(upload_to=settings.THUMBNAIL_DIRECTORY)
     num_frames = models.IntegerField()
     framerate = models.FloatField()
     date_created = models.DateTimeField(auto_created=True, auto_now=True, editable=False)
-    tags = models.ManyToManyField(Tag, related_name='videos')
+    tags = models.ManyToManyField(Tag, related_name='videos', default=[])
