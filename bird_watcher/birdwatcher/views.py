363d3b5941f6692bfe272c0832be899797c122ba
Stephane Latil <stephanelatil@gmail.com>
2024-03-18 13:39:12 +0100

    Makes streaming seek-capable

--- bird_watcher/birdwatcher/views.py
+++ bird_watcher/birdwatcher/views.py
@@ -3,17 +3,22 @@ from birdwatcher.models import Video, Tag
 from birdwatcher.forms import TagVideoForm
 from django.views.generic import View, ListView, DetailView
 from django.http import HttpRequest
-from os import stat, path, SEEK_SET
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseNotModified
 from django.views.generic.edit import FormMixin
 from json import loads
-from fastapi import APIRouter
-from fastapi.responses import StreamingResponse, FileResponse, Response
+from fastapi import APIRouter, Header, HTTPException, Request, status
+from fastapi.responses import StreamingResponse, FileResponse, Response, RedirectResponse
 from multiprocessing import Condition
 from threading import Thread
+from asgiref.sync import sync_to_async
+from anyio import open_file
+import os
+from typing import BinaryIO
+from starlette._compat import md5_hexdigest
+from datetime import datetime
 
 api_router = APIRouter()
 
@@ -178,34 +183,112 @@ class LiveStreamVideo:
         return LiveStreamVideo._singelton
     
     @staticmethod
-    def livestream_frame_generator():
+    async def livestream_frame_generator():
         stream = LiveStreamVideo.getSingleton()
         while stream.unread_frames < LiveStreamVideo._max_unread_frames:
             yield stream.get_frame()
         raise StopIteration()
 
-@api_router.get('/stream/{pk}')
-def stream_video_file(pk=None):
-    def iter_file(file_path):
-        CHUNK_SIZE = 1024*128 #128Kib
-        with open(file_path, 'rb') as vid_file:
-            while content := vid_file.read(CHUNK_SIZE):
-                yield content
-    
-    try:
-        vid = get_object_or_404(Video.objects.all(), pk=pk)
-    except:
-        return Response(status_code=404)
-    return StreamingResponse(iter_file(vid.video_file))
-        
-
 @api_router.get('/stream/live')
 async def livestream_video():
     if len(settings.STREAM_VID_DEVICE) == 0:
-        return FileResponse(settings.STATICFILES_DIRS[0]/'no-stream.jpg')
+        return RedirectResponse('/static/no-stream.jpg')
     return StreamingResponse(LiveStreamVideo.livestream_frame_generator(),
                       media_type="multipart/x-mixed-replace;boundary=frame")
+
+def send_bytes_range_requests(file_obj: BinaryIO,
+                              start: int, end: int, chunk_size: int = 128*1024):
+    """Send a file in chunks using Range Requests specification RFC7233
+    `start` and `end` parameters are inclusive due to specification
+    """
+    with file_obj as f:
+        f.seek(start)
+        while (pos := f.tell()) <= end:
+            read_size = min(chunk_size, end + 1 - pos)
+            yield f.read(read_size)
+
+
+def _get_range_header(range_header: str, file_size: int) -> tuple[int, int]:
+    def _invalid_range():
+        return HTTPException(
+            status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE,
+            detail=f"Invalid request range (Range:{range_header!r})")
+
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError:
+        raise _invalid_range()
+
+    if start > end or start < 0 or end > file_size - 1:
+        raise _invalid_range()
+    return start, end
+
+
+def range_requests_response(
+    request: Request, file_path: str, content_type: str
+):
+    """Returns StreamingResponse using Range Requests of a given file"""
+
+    file_size = os.stat(file_path).st_size
+    range_header = request.headers.get("range")
+
+    """Compose etag from last_modified and file_size"""
+    last_modified = datetime.fromtimestamp(os.stat(file_path).st_mtime).strftime("%a, %d %b %Y %H:%M:%S")
+    etag_base = str(last_modified) + "-" + str(file_size)
+    etag = f'"{md5_hexdigest(etag_base.encode(), usedforsecurity=False)}"'
+
+    """Check if the browser sent etag matches the videos etag"""
+    request_if_non_match_etag = request.headers.get("if-none-match")
+
+    """if there is a match return 304 unmodified instead of 206 response without video file"""
+    if request_if_non_match_etag == etag:
+        headers = {
+            "cache-control": "public, max-age=86400, stale-while-revalidate=2592000",
+            "etag" : etag,
+            "last-modified":str(last_modified),
+        }
+        status_code = status.HTTP_304_NOT_MODIFIED
+        return Response(None, status_code=status_code, headers=headers)
+
+    headers = {
+        "etag" : etag,
+        "content-type": content_type,
+        "accept-ranges": "bytes",
+        "content-encoding": "identity",
+        "content-length": str(file_size),
+        "access-control-expose-headers": (
+            "content-type, accept-ranges, content-length, "
+            "content-range, content-encoding"
+        ),
+    }
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header, file_size)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+    else:
+        start, end = 0, file_size - 1
+        status_code = status.HTTP_200_OK        
+
+    return StreamingResponse(
+        send_bytes_range_requests(open(file_path, mode="rb"), start, end),
+        headers=headers,
+        status_code=status_code,
+    )
+
+@api_router.get('/stream/{pk}')
+async def stream_video_file(request:Request, pk:int):
+    try:
+        vid = await sync_to_async(get_object_or_404)(Video.objects.all(), pk=pk)
+    except:
+        return Response(status_code=404)
     
+    return range_requests_response(request, vid.video_file, content_type='media/mp4')
+
 @api_router.get("/favicon.ico")
 async def get_favicon():
     # Step 6: Return the favicon.ico file using FileResponse
