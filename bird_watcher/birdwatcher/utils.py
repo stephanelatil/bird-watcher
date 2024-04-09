81dfeb235ecf587831f9e549ec75d13cc231bb20
Stephane Latil <stephanelatil@gmail.com>
2024-04-09 10:59:46 +0200

    Adds colors for console logging

--- bird_watcher/birdwatcher/utils.py
+++ bird_watcher/birdwatcher/utils.py
@@ -38,15 +38,46 @@ def kill_and_restart_birdwatcher(start_new_birdwatcher=True):
     else:
         #just kills birdwatcher
         Thread(target=_kill_birdwatcher, daemon=True).start()
+        
+class ColoredSimpleFormatter(logging.Formatter):
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    red = "\x1b[31m"
+    bold_red = "\x1b[31m"
+    reset = "\x1b[0m"
+
+    FORMATS = {
+        logging.DEBUG: green,
+        logging.INFO: green,
+        logging.WARNING: yellow,
+        logging.WARN: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: bold_red,
+        logging.ERROR: bold_red,
+        logging.FATAL: bold_red
+    }
+    
+    def __init__(self, fmt: str | None = None, datefmt: str | None = None,
+                 validate: bool = True, *, defaults=None) -> None:
+        self.style:logging._FormatStyle = '{'
+        self.validate = validate
+        super().__init__(fmt, datefmt, style=self.style, validate=validate, defaults=defaults)
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        fmt = self._fmt.replace('{levelname}', rf'{log_fmt}{{levelname}}{self.reset}')
+        return logging.Formatter(fmt,
+                                 datefmt=self.datefmt,
+                                 style=self.style,
+                                 validate=self.validate
+                                 ).format(record)
 
 def setup_logging():
     root_logger = logging.getLogger()
     
     #create formatters here
-    
-    simple_formatter = logging.Formatter("[{levelname}]: {message}",
-                               datefmt="%Y/%m/%d-T%H:%M:%S%z",
-                               style='{')
+    simple_formatter = ColoredSimpleFormatter("{levelname}: {message}",
+                               datefmt="%Y/%m/%d-T%H:%M:%S%z")
     detailed_formatter = logging.Formatter("[{levelname}|{filename}|L_{lineno}] {asctime}: {message}",
                                datefmt="%Y/%m/%d-T%H:%M:%S%z",
                                style='{')
