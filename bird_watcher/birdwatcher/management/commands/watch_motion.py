eb47a5f6d98aee36e4991b2f91704af8c8c9a090
Stephane Latil <stephanelatil@gmail.com>
2024-04-14 20:28:38 +0200

    Increases pixel change trigger threshold

--- bird_watcher/birdwatcher/management/commands/watch_motion.py
+++ bird_watcher/birdwatcher/management/commands/watch_motion.py
@@ -182,7 +182,7 @@ class MotionDetector:
         
         diff = cv2.absdiff(self._background, frame)
         self.update_backgroud(frame)
-        _,diff = cv2.threshold(diff, 45, 255, cv2.THRESH_BINARY)
+        _,diff = cv2.threshold(diff, 60, 255, cv2.THRESH_BINARY)
         num_pixels_theshold = self._mov_on_frame_amount*diff.shape[0]*diff.shape[1]
         nonZero = cv2.countNonZero(diff)
         if nonZero > num_pixels_theshold/2 and not nonZero > num_pixels_theshold:
