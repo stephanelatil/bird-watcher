c26af1f628ac4a3875d48dd603618292eb7f364b
Stephane Latil <stephanelatil@gmail.com>
2024-06-11 13:12:04 +0200

    Fixes warning

--- bird_watcher/bird_watcher_proj/urls.py
+++ bird_watcher/bird_watcher_proj/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path(r'', include(router.urls)),
     re_path('config/?', ConfigView.as_view(), name=ConfigView.url_name),
     re_path('video/?', VideoListView.as_view(), name=VideoListView.url_name),
-    path('video/(?P<pk>[1-9][0-9]*)/tag', VideoTagView.as_view(), name=VideoTagView.url_name),
+    re_path('video/(?P<pk>[1-9][0-9]*)/tag', VideoTagView.as_view(), name=VideoTagView.url_name),
     re_path('video/(?P<pk>[1-9][0-9]*)', SingleVideoView.as_view(), name='video-detail'),
     re_path('livestream/?', LiveStreamView.as_view(), name='video-livestream'),
     path(r'/', RedirectView.as_view(url='video', permanent=True), name='index')
