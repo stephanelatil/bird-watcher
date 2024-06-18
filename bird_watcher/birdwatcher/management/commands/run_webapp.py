a752940dc88c29db0dd8572ff3480a063f490d20
Stephane Latil <stephanelatil@gmail.com>
2024-06-18 10:00:18 +0200

    Enables motion detection autostart toggle

--- bird_watcher/birdwatcher/management/commands/run_webapp.py
+++ bird_watcher/birdwatcher/management/commands/run_webapp.py
@@ -1,6 +1,7 @@
 from django.conf import settings
+from constance import config
 from django.core.management import BaseCommand
-from birdwatcher.utils import setup_logging
+from birdwatcher.utils import setup_logging, start_or_restart_birdwatcher
 import uvicorn
 
 class Command(BaseCommand):
@@ -9,6 +10,8 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         setup_logging()
+        if config.START_MOTION_DETECTOR_ON_SERVER_START:
+            start_or_restart_birdwatcher()
         uvicorn.run('bird_watcher_proj.asgi:app',
                     host=settings.WEBAPP_HOST,
                     port=settings.WEBAPP_PORT,
